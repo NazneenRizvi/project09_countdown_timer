c73e4d91e18ea26cb1f206b312f472cd9954bfc3
NazneenRizvi <149296219+NazneenRizvi@users.noreply.github.com>
2023-11-07 14:42:23 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import readline from 'readline';
+import { format, subSeconds } from 'date-fns';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function formatTime(seconds: number): string {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+}
+
+function countdownTimer(totalSeconds: number) {
+  let remainingTime = totalSeconds;
+
+  const timer = setInterval(function () {
+    if (remainingTime <= 0) {
+      clearInterval(timer);
+      console.log('Timer has expired');
+      rl.close();
+    } else {
+      const formattedTime = formatTime(remainingTime);
+      console.log(formattedTime);
+      remainingTime -= 1;
+    }
+  }, 1000);
+}
+
+rl.question('Please enter the amount of seconds: ', (input: string) => {
+  const totalSeconds = parseInt(input);
+  if (isNaN(totalSeconds) || totalSeconds <= 0) {
+    console.log('Please enter a valid positive number of seconds.');
+    rl.close();
+  } else {
+    countdownTimer(totalSeconds);
+  }
+});
+
+rl.on('close', () => {
+  process.exit(0);
+});
